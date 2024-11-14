d7d541aebb0a8e0be9a3cd9542ee958b22f6e1e0
bhumika-ios <bhumika.iosdeveloper.com>
2024-11-14 10:36:58 +0530

    Initial Commit

---
+++ ButtonSwipeApp/ButtonSwipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonSwipeAppApp.swift
+//  ButtonSwipeApp
+//
+//  Created by Bhumika Patel on 14/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ButtonSwipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ButtonSwipeAppTests/ButtonSwipeAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  ButtonSwipeAppTests.swift
+//  ButtonSwipeAppTests
+//
+//  Created by Bhumika Patel on 14/11/24.
+//
+
+import Testing
+
+struct ButtonSwipeAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
